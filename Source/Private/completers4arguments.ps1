6fa23c40f12319863dbc833d40748fcd9e09ad54
Donovan Brown <dbrown@microsoft.com>
2021-02-07 18:11:43 -0600

    adding upgrade

--- Source/Private/completers4arguments.ps1
+++ Source/Private/completers4arguments.ps1
@@ -5,6 +5,7 @@ Register-Completer dapr_stop_-a $instancesAll
 Register-Completer dapr_stop_--app-id $instancesAll
 Register-Completer dapr_instance_stop $instancesAll
 Register-Completer dapr_init_--runtime-version $releasesAll
+Register-Completer dapr_upgrade_--runtime-version $releasesAll
 
 # You can omit the -a or --app-id and just provide the instance
 Register-Completer dapr_stop (Get-Completer dapr_instance_stop)
